1ae454b5c1d66cb73bbc70950f8208c280b73423
Maizekeen <46721252+IAMN1@users.noreply.github.com>
2025-06-26 20:46:51 +0300

    Delete Client_V_1.5 directory

--- Client_V_1.5/Utils/helpers.py
+++
@@ -1,7 +0,0 @@
-def load_stylesheet(file_path="Resources/styles.qss"):
-    with open(file_path, "r") as f:
-        return f.read()
-
-def emit_signal(signal_name):
-    from Utils.signal_manager import signal_manager
-    signal_manager.navigate.emit(signal_name)
